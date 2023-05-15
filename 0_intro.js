b8f97e8b386657af5ee7d0c678bd2d8c6f5e10b7
siba shankar sahu <sibashankar255@gmail.com>
2023-05-15 15:55:44 +0530

    javascript day 1

--- 0_intro.js
+++ 0_intro.js
@@ -1,2 +1,9 @@
 //datatype
 //hello world
+
+//git commands
+//initializating a git in project folder- git init
+//add all the files from that folder - git add .
+//if adding one file - git add file_name
+//commit all your changes - git commit -m "give the meaningful message"
+//push all your changes - git push
---
+++ JavaScript-1/day1.js
@@ -0,0 +1,9 @@
+
+// user Interface -> Browser Engine -> Rendering Engine -> Networking -> Js Interpretor -> UI Backend -> Data Storage
+
+// Html-> to provide a structure
+// css-> to style/design the structure
+//javascript-> to add functionality/ to be lively
+
+//DOM(Document object model)-> tree like structure representing the HTML elements in the browser.
+//CSSOM(CSS object model)-> tree like structure representing the css properties on each element.
